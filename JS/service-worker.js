a1058aaf20b85b13f8c4a9e2712a2c955e1473a1
HuzaifaMustafa123 <97903294+HuzaifaMustafa123@users.noreply.github.com>
2023-02-07 10:28:29 +0400

    Update service-worker.js

--- JS/service-worker.js
+++ JS/service-worker.js
@@ -33,3 +33,15 @@ self.addEventListener('install', (e) => {
 //             })
 //         );
 //     });
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+     cathes.match(e.request).then(function (r) {
+        return r || fetch(e.request).then(function (response) {
+            return caches.open(cacheName).then(function (cache){
+                cache.put(e.request, response.clone());
+            });
+        });
+    })
+    );
+});
